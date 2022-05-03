38bce2a473b1cb0dd00ddbea0372c34028698ac2
JimmyMahendra <104680176+JimmyMahendra@users.noreply.github.com>
2022-05-03 18:12:25 +0700

    Update and rename kyyhelper.py to clownhelper.py

--- userbot/modules/clownhelper.py
+++ userbot/modules/clownhelper.py
@@ -1,28 +1,28 @@
 """ Userbot module for other small commands. """
 from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
-from userbot.utils import edit_or_reply, kyy_cmd
+from userbot.utils import edit_or_reply, clown_cmd
 
 
-@kyy_cmd(pattern="lhelp$")
+@clown_cmd(pattern="lhelp$")
 async def usit(e):
     await edit_or_reply(e,
                         f"**Halo {owner} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `{cmd}help` Atau Bisa Minta Bantuan Ke:\n"
-                        "\n[Telegram](t.me/IDnyaKosong)"
-                        "\n[Repo](https://github.com/S/Kyy-Userbot)"
-                        "\n[Instagram](instagram.com/rizkyhamdanii16_)")
+                        "\n[Telegram](t.me/IamKitaro)"
+                        "\n[Repo](https://github.com/JimmyMahendra/Clown-Userbot)"
+                        "\n[Instagram](instagram.com/jimmymhndra)")
 
 
-@kyy_cmd(pattern="vars$")
+@clown_cmd(pattern="vars$")
 async def var(m):
     await edit_or_reply(m,
                         f"**Disini Daftar Vars Dari {owner}:**\n"
-                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/muhammadrizky16/Kyy-Userbot/Kyy-Userbot/varshelper.txt)")
+                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/JimmyMahendra/Clown-Userbot/Clown-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "helper":
     f"`{cmd}lhelp`\
-\nUsage: Bantuan Untuk Kyy-Userbot.\
+\nUsage: Bantuan Untuk Clown-Userbot.\
 \n`{cmd}vars`\
 \nUsage: Melihat Daftar Vars."
 })
